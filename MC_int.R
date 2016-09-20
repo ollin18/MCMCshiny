b150c26cb48f297ace630bfe1ceb1327fa2d0f07
Ollin Demian <ollin.langle@ciencias.unam.mx>
2016-09-19 19:25:34 -0500

    Código limpio

--- MC_int.R
+++ MC_int.R
@@ -1,5 +1,3 @@
-f <- function() x^2
-
 integral_MC <- function(f,a,b,N=10000){
   
   al_cero <- function(x){dist(c(x,0))}
@@ -14,14 +12,7 @@ integral_MC <- function(f,a,b,N=10000){
   dist_fun <- sapply(funcion_aplicada,al_cero)
   dist_com <- sapply(comparada,al_cero)
   
-  unos <- function(x){
-    1
-  }
-  
   bajo_la_curva <- sum(dist_fun > dist_com & (sign(comparada)==1 & sign(funcion_aplicada)==1))
   sobre_la_curva <- sum(dist_fun > dist_com & (sign(comparada)==-1 & sign(funcion_aplicada)==-1))
   (bajo_la_curva - sobre_la_curva)/N * abs(max(maximo,0)-min(minimo,0))*abs(b-a)
 }
-
-integrate(function(x) cos(x),-22,55)
-integral_MC(function(x) cos(x),-22,55)
